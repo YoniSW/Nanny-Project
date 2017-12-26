f9f1e2ee02992dcad2dcda5a4522b67a63b1f2e7
YoniSW <33528511+YoniSW@users.noreply.github.com>
2017-12-26 22:11:37 +0200

    PL test WPF

--- PLWPF/MainWindow.xaml.cs
+++ PLWPF/MainWindow.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PL
+namespace PLWPF
 {
     /// <summary>
-    /// Interaction logic for Nanny.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class Nanny : Window
+    public partial class MainWindow : Window
     {
-        public Nanny()
+        public MainWindow()
         {
             InitializeComponent();
         }
